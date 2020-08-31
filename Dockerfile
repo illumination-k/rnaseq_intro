afdb0e863f7a52bbcf27e447ee665c37a65d6e74
illumination-k <illumination.k.27@gmail.com>
2020-08-31 06:14:42 +0000

    add files

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+FROM conda/miniconda3
+
+# r-notebookを使うとDESeq2とのconflictが起こる
+
+USER root
+
+RUN apt-get update --fix-missing && \
+    apt-get install -y less git && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+# black background
+RUN mkdir -p /home/jovyan/.jupyter/lab/user-settings/@jupyterlab/apputils-extension
+RUN echo '{"theme":"JupyterLab Dark"}' > \
+        /home/jovyan/.jupyter/lab/user-settings/@jupyterlab/apputils-extension/themes.jupyterlab-settings
+
+RUN conda update -n base conda 
+
+RUN conda install -c bioconda -c conda-forge -c r \
+        jupyterlab \
+        r-irkernel \
+        bioconductor-deseq2 \
+        bioconductor-clusterprofiler
+
+RUN pip install \
+        pandas \
+        numpy \
+        matplotlib \
+        seaborn \
+        scipy \
+        scikit-learn \
+        clust \
+        openpyxl
+
+USER jovyan
--- pipeline.sh
+++ pipeline.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-docker run --rm -it -v $(pwd):/local_volume illumination27/sra-tools:latest \
-	fasterq-dump SRR5826942 -e 6
+for f in $(cat SRR_Acc_List.txt); do
+    docker run --rm -it -v $(pwd):/local_volume illumination27/sra-tools:latest \
+        fasterq-dump ${f} -e 6
 
-docker run --rm -it -v $(pwd):/local_volume illumination27/fastp:latest \
-	fastp -i SRR5826942.fastq -o SRR5826942_trim.fastq.gz -j SRR5826942.json -h SRR5826942.html -w 6
+    docker run --rm -it -v $(pwd):/local_volume illumination27/fastp:latest \
+        fastp -i ${f}.fastq -o ${f}_trim.fastq.gz -j ${f}.json -h ${f}.html -w 6
 
-docker run --rm -it -v $(pwd):/local_volume illumination27/salmon-tximport:latest \
-	salmon quant -i ./arabidopsis_ref/salmon_index \
-	-l A -r SRR5826942_trim.fastq.gz \
-	-p 6 -o SRR5826942_exp
+    docker run --rm -it -v $(pwd):/local_volume illumination27/salmon-tximport:latest \
+        salmon quant -i ./arabidopsis_ref/salmon_index \
+        -l A -r ${f}_trim.fastq.gz \
+        -p 6 -o ${f}_exp
+done
